6cffaf07692c827ceea8d2ee1063d3c4d30ece00
teddypickerfromul <teddypickerfromul@gmail.com>
2012-06-05 19:30:23 +0400

    Update master

--- noty.py
+++ noty.py
@@ -6,7 +6,6 @@ import subprocess
 
 stat = os.statvfs("/")
 amount = (((stat.f_bsize * stat.f_bavail)/1024)/1024)
-#if(amount < 600):
 msg = "Where are only "+str(amount)+" Mb"
 subprocess.call(['notify-send',"Aсhtung!!!",msg,"-i"," notification-message-warning","-t","-3000"])
 		
