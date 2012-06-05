28e9d9322e4d523037bf75675a8579cf5966aab2
teddypickerfromul <teddypickerfromul@gmail.com>
2012-06-05 19:29:42 +0400

    Update master

--- noty.py
+++ noty.py
@@ -8,10 +8,7 @@ stat = os.statvfs("/")
 amount = (((stat.f_bsize * stat.f_bavail)/1024)/1024)
 #if(amount < 600):
 msg = "Where are only "+str(amount)+" Mb"
-subprocess.call(['notify-send',"Ahtung!!!",msg,"-i"," notification-message-warning","-t","-3000"])
-	#if(amount < 100):
-		#head = "Oh SHI~"  
-		#subprocess.call(['notify-send',head,msg, "-t","-3000"])
+subprocess.call(['notify-send',"Aсhtung!!!",msg,"-i"," notification-message-warning","-t","-3000"])
 		
 
 
